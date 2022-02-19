1ee606edb28cca15676982fb90838bd851a16feb
vmwhoami <vitalie.m@travod.com>
2022-02-19 20:24:00 +0200

    First commit

---
+++ contracts/Inbox.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.7;
+
+contract FistContract {
+    string public message;
+
+    function Inbox(string memory initialMessage) public {
+    	message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+    	message = newMessage;
+    }
+}
